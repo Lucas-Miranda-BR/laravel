1228b74eee438e187f4831ef90f38776d59a66a0
Lucas-Miranda-BR <lucasmirandabr0508@gmail.com>
2026-03-03 14:48:57 -0300

    Reset main

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,7 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::get('/principal', [App\Http\Controllers\Principal::class, 'principal']);
-Route::get('/principal/sobre', [App\Http\Controllers\Sobre::class, 'sobreEmpressa']);
-Route::get('/principal/contato', [App\Http\Controllers\Contato::class, 'contato']);
-Route::get('/principal/contrato', [App\Http\Controllers\Contrato::class, 'contrato']);
-Route::get('/usuario', [App\Http\Controllers\Usuario::class, 'usuario']);
-Route::get('/usuario/configuracao', [App\Http\Controllers\Configuracao::class, 'configuracao']);
-Route::get('/funcionario', [App\Http\Controllers\Funcionario::class, 'funcionarioInformacao']);
-Route::get('/funcionario/trabalho', [App\Http\Controllers\FuncionarioTrabalho::class, 'areaTrabalho']);
-Route::get('/admin', [App\Http\Controllers\Admin::class, 'admin']);
-Route::get('/admin/usuario', [App\Http\Controllers\AdminUsuario::class, 'gerenciarUsuario']);
-Route::get('/admin/produto', [App\Http\Controllers\AdminProduto::class, 'gerenciarProduto']);
+Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
 
 
 
