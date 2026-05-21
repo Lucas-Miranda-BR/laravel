810672c8112b5c3f1bb958399c142ffdb89d6b96
Lucas-Miranda-BR <lucasmirandabr0508@gmail.com>
2026-05-21 16:33:31 -0300

    typo fix

--- app/Models/ProfessorModel.php
+++ app/Models/ProfessorModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CursoProfessor extends Model
+class ProfessorModel extends Model
 {
     use HasFactory;
 
